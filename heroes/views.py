3270d95b4f081ec88e3650dd8f4e9cda8e8d2e52
Pramish K.C <pramishkc066@gmail.com>
2025-07-06 18:58:34 +0545

    superuser login/logout

--- heroes/views.py
+++ heroes/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import SuperHeroes
 from .forms import SuperheroForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
+
+@login_required
 def home(request):
     return render(request, 'heroes/home.html')
 
+
+@login_required
 def hero_list(request):
     heroes = SuperHeroes.objects.all()
     return render(request, 'heroes/hero_list.html', {'heroes': heroes})
 
+
+@login_required
 def hero_detail(request, id):
     hero = get_object_or_404(SuperHeroes, id=id)
     return render(request, 'heroes/hero_detail.html', {'hero': hero})
 
+
+@login_required
 def add_hero(request):
     if request.method == 'POST':
         form = SuperheroForm(request.POST)
@@ -23,7 +33,14 @@ def add_hero(request):
         form = SuperheroForm()
     return render(request, 'heroes/add_hero.html', {'form': form})
 
+
+@login_required
 def delete_hero(request, id):
     hero = get_object_or_404(SuperHeroes, id=id)
     hero.delete()
     return redirect('hero_list')
+
+
+@login_required
+def logout_view(request):
+    return logout(request)
